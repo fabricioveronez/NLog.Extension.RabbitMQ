9b2d40c21289d1ad5a31794fea719d40aee15483
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-05-06 01:03:39 -0300

    WebApi Example Project

---
+++ Examples/NLog.Extension.RabbitMQ.Examples.WebApi/Controllers/LogController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog.Extension.RabbitMQ.Examples.WebApi.Model;
+
+namespace NLog.Extension.RabbitMQ.Examples.WebApi.Controllers
+{
+    [Route("api/log")]
+    public class LogController : Controller
+    {
+
+        private ILogger<LogController> _log;
+
+        public LogController(ILogger<LogController> log)
+        {
+            this._log = log;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Log log)
+        {
+            switch (log.LogLevel)
+            {
+                case Model.LogLevel.Trace:
+                    this._log.LogTrace(log.Message);
+                    break;
+                case Model.LogLevel.Debug:
+                    this._log.LogDebug(log.Message);
+                    break;
+                case Model.LogLevel.Info:
+                    this._log.LogInformation(log.Message);
+                    break;
+                case Model.LogLevel.Warn:
+                    this._log.LogWarning(log.Message);
+                    break;
+                case Model.LogLevel.Error:
+                    this._log.LogError(log.Message);
+                    break;
+                case Model.LogLevel.Fatal:
+                    this._log.LogCritical(log.Message);
+                    break;              
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Examples/NLog.Extension.RabbitMQ.Examples.WebApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NLog.Extension.RabbitMQ.Examples.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // NLog: setup the logger first to catch all errors
+            var logger = Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("Init Main");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+             WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
