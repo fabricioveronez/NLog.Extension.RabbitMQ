e1f85da41e91241fb8752a998a07a343129775fe
Fabricio Veronez <>
2018-06-05 11:40:09 -0300

    Version 1.0.2

--- NLog.Extension.RabbitMQ.Target/RabbitMQTarget.cs
+++ NLog.Extension.RabbitMQ.Target/RabbitMQTarget.cs
@@ -1,6 +1,8 @@
-﻿using NLog.Config;
+﻿using NLog.Common;
+using NLog.Config;
 using NLog.Targets;
 using RabbitMQ.Client;
+using System;
 using System.Text;
 
 namespace NLog.Extension.RabbitMQ.Target
@@ -25,35 +27,42 @@ namespace NLog.Extension.RabbitMQ.Target
         [RequiredParameter]
         public string Password { get; set; }
 
-        public int Port { get; set; }       
+        public int Port { get; set; }
 
         public string Exchange { get; set; }
 
         [RequiredParameter]
         public string RoutingKey { get; set; }
 
-        public string VirtualHost { get; set; }        
+        public string VirtualHost { get; set; }
 
         protected override void Write(LogEventInfo logEvent)
         {
             string logMessage = this.Layout.Render(logEvent);
 
-            var factory = new ConnectionFactory() { HostName = this.HostName, VirtualHost = this.VirtualHost, Port = this.Port, UserName = this.UserName, Password = this.Password };
-            using (var connection = factory.CreateConnection())
+            try
             {
-                using (var channel = connection.CreateModel())
-                {                
-                    var body = Encoding.UTF8.GetBytes(logMessage);
+                var factory = new ConnectionFactory() { HostName = this.HostName, VirtualHost = this.VirtualHost, Port = this.Port, UserName = this.UserName, Password = this.Password };
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        var body = Encoding.UTF8.GetBytes(logMessage);
 
-                    IBasicProperties props = channel.CreateBasicProperties();
-                    props.ContentType = "text/plain";
-                    props.DeliveryMode = 2;
+                        IBasicProperties props = channel.CreateBasicProperties();
+                        props.ContentType = "text/plain";
+                        props.DeliveryMode = 2;
 
-                    channel.BasicPublish(exchange: this.Exchange,
-                                         routingKey: this.RoutingKey,
-                                         basicProperties: null,
-                                         body: body);
+                        channel.BasicPublish(exchange: this.Exchange,
+                                             routingKey: this.RoutingKey,
+                                             basicProperties: null,
+                                             body: body);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                InternalLogger.Error("{ Could not send to RabbitMQ: {0} Log: {1} }", ex.ToString(), logMessage);
             }
         }
     }
